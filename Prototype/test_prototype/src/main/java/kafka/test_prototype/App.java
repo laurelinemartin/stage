f1b2833a3a867aa0923dfcfcd9c7856807afc2df
Laureline Martin <laureline.martin@ens.uvsq.fr>
2020-09-03 18:57:19 +0200

    Création du projet maven pour le premier prototype

---
+++ Prototype/test_prototype/src/main/java/kafka/test_prototype/App.java
@@ -0,0 +1,13 @@
+package kafka.test_prototype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
