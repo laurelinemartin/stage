e692acaa35aa93d7d51609d50fc58599a26c9247
Laureline Martin <laureline.martin@ens.uvsq.fr>
2020-09-14 12:46:13 +0200

    Modification du main

--- Prototype/prototype/prototype/src/main/java/com/main/App.java
+++ Prototype/prototype/prototype/src/main/java/com/main/App.java
@@ -1,9 +1,4 @@
 package com.main;
-import java.io.IOException;
-import java.util.List;
-
-import com.IO.file.LireSouscription;
-import com.consumer.*;
 import com.producer.*;
 
 /**
@@ -16,23 +11,6 @@ public class App
 	public static void main( String[] args )
     {
         System.out.println( "Message du producer \n ----------------\n" );
-        ProducerP prod = new ProducerP("test");
-    	if(args[0].equals("-file")) {
-    		LireSouscription ls = new LireSouscription(args[1]);
-    		List<String> topics = null;
-			try {
-				topics = ls.topics();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-    		System.out.println( "Message du consumer \n ----------------\n" );
-            for(String topic : topics) {
-            	System.out.println(topic);
-            	ConsumerC cons = new ConsumerC(topic);
-            }
-    	}
-    	else {
-    		System.out.println("Pas de souscription\n");
-    	}
+        ProducerP prod = new ProducerP(args[0]);
     }
 }
